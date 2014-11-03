6f1608c8aa7b59bfc127c87246e77d75eb747c2a
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-03 18:28:54 -0500

    basic objects created.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,29 +1,30 @@
-class Player
-	attr_reader :name, :mark
-	def initialize(name, mark)
-		@name = name
-		@mark = mark
-	end
-end
-
-
-
-class Board
-	attr_accessor :grid
-	def initialize
-		@grid = Array.new(9) { " " } #cells numbered 0 to 9 starting at top left
-	end
-end
-
-
-
 class Game
-	attr_accessor :player_one, :player_two
+	attr_accessor :player_one, :player_two, :board
 	def initialize
-    @player_one = Player.new("Player 1", "X")
+		@player_one = Player.new("Player 1", "X")
     @player_two = Player.new("Player 2", "O")
     @board = Board.new
   end
-end
 
+
+  class Player
+  	attr_accessor :name, :mark
+  	def initialize(name, mark)
+  		@name = name
+  		@mark = mark
+  	end
+  end
+
+
+
+  class Board
+  	attr_accessor :grid
+  	def initialize
+  		@grid = Array.new(9) { " " } #cells numbered 0 to 9 starting at top left
+  	end
+  end
+
+
+
+end
 
