f0ac3a9ab3b44e25fe4db2da952b448351892477
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-03 15:57:03 -0500

    some more basic work.  don't know what I'm doing....

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,18 +1,29 @@
 class Player
+	attr_reader :name, :mark
 	def initialize(name, mark)
-		@player = {name => mark}
+		@name = name
+		@mark = mark
 	end
 end
 
 
 
 class Board
+	attr_accessor :grid
 	def initialize
-		@grid = Array(9) { " " } #cells numbered 0 to 9 starting at top left
+		@grid = Array.new(9) { " " } #cells numbered 0 to 9 starting at top left
 	end
-  	
-
 end
 
+
+
+class Game
+	attr_accessor :player_one, :player_two
+	def initialize
+    @player_one = Player.new("Player 1", "X")
+    @player_two = Player.new("Player 2", "O")
+    @board = Board.new
+  end
+end
 
 
