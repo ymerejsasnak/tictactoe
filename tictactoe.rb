cc9f701ccf1d6042e75b12023acb23ae85d3aeef
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-03 15:40:47 -0500

    basic structure begun

---
+++ tictactoe.rb
@@ -0,0 +1,18 @@
+class Player
+	def initialize(name, mark)
+		@player = {name => mark}
+	end
+end
+
+
+
+class Board
+	def initialize
+		@grid = Array(9) { " " } #cells numbered 0 to 9 starting at top left
+	end
+  	
+
+end
+
+
+
