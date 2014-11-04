7c59a5102bf29c77d16ccfec67a2b4c8e57e0579
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-03 23:57:56 -0500

    still working on basics

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,30 +1,51 @@
+class Player
+	attr_accessor :name, :mark
+	def initialize(name, mark)
+		@name = name
+ 		@mark = mark
+ 	end
+
+end
+
+
+
+class Board
+ 	attr_accessor :grid
+ 	def initialize
+ 		@grid = Array.new(9) { " " } #cells numbered 0 to 9 starting at top left
+ 	end
+
+ 	def show_board
+ 		puts
+ 		puts "   #{@grid[0]} | #{@grid[1]} | #{@grid[2]} "
+ 		puts " ------------- "
+ 		puts "   #{@grid[3]} | #{@grid[4]} | #{@grid[5]} "
+ 		puts " ------------- "
+ 		puts "   #{@grid[6]} | #{@grid[7]} | #{@grid[8]} "
+ 		puts
+ 	end
+
+end
+
+
+
 class Game
-	attr_accessor :player_one, :player_two, :board
+	attr_accessor :player_one, :player_two, :board, :current_player
 	def initialize
 		@player_one = Player.new("Player 1", "X")
     @player_two = Player.new("Player 2", "O")
     @board = Board.new
+    @current_player = @player_one
   end
 
+  def mark_cell(cell)
+    board.grid[cell] = current_player.mark
+    board.show_board
+ 	end
 
-  class Player
-  	attr_accessor :name, :mark
-  	def initialize(name, mark)
-  		@name = name
-  		@mark = mark
-  	end
+  def game_loop
   end
-
-
-
-  class Board
-  	attr_accessor :grid
-  	def initialize
-  		@grid = Array.new(9) { " " } #cells numbered 0 to 9 starting at top left
-  	end
-  end
-
-
 
 end
 
+g = Game.new
