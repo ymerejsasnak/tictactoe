75bf3a345b4e936724c9e3a1d4a2d06e75a673a4
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-04 10:51:41 -0500

    lots of work without a commit :(  but getting there...

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,10 +1,10 @@
 class Player
-	attr_accessor :name, :mark
+	attr_accessor :name, :mark, :moves
 	def initialize(name, mark)
 		@name = name
  		@mark = mark
+    @moves = []
  	end
-
 end
 
 
@@ -24,28 +24,74 @@ class Board
  		puts "   #{@grid[6]} | #{@grid[7]} | #{@grid[8]} "
  		puts
  	end
-
 end
 
 
 
 class Game
-	attr_accessor :player_one, :player_two, :board, :current_player
-	def initialize
+	attr_accessor :player_one, :player_two, :board, :current_player, :win_conditions
+
+  def initialize
 		@player_one = Player.new("Player 1", "X")
     @player_two = Player.new("Player 2", "O")
     @board = Board.new
-    @current_player = @player_one
+    @current_player = player_one
+    @win_conditions = [[0,1,2], [3,4,5], [6,7,8],
+                      [0,3,6], [1,4,7], [2,5,8],
+                      [0,4,8], [2,4,6]]
   end
 
-  def mark_cell(cell)
-    board.grid[cell] = current_player.mark
-    board.show_board
+  def mark_cell(cell, player)
+    board.grid[cell] = player.mark
+    player.moves << cell
  	end
 
+  def get_input(player)
+    begin
+      puts
+      print "#{player.name} please choose a cell (0 - 9): "
+      input = gets.chomp.to_i
+    end until valid?(input)
+    return input
+  end
+
+  def valid?(cell)
+    if board.grid[cell] == " "
+      return true
+    else
+      return false
+    end
+  end
+
+  def end?(player)
+    if winner?(player)
+      puts "You win #{player.name}!"
+    end
+    #if draw?
+    #end
+  end
+
+  def winner?(player)
+    win_conditions.any? {|cells| player.marks.include?(cells)}    
+  end
+
+
   def game_loop
+
+    loop do  
+      board.show_board
+      mark_cell(get_input(player_one), player_one)
+      break if end?(player_one)
+      board.show_board
+      mark_cell(get_input(player_two), player_two)
+      break if end?(player_two)
+    end
+
   end
 
 end
 
+
+
 g = Game.new
+g.game_loop
